0f291f76442bd3f889df47bd5484865395cfeb48
Yelaman Moldagali <115030149+yelagman@users.noreply.github.com>
2024-09-28 00:31:13 -0400

    Created an Address class
    Created an Address class, addressing the issue #1. Updated the implementation of billing and shipping setter methods in User.java with newly made Address class.

---
+++ src/Address.java
@@ -0,0 +1,32 @@
+public class Address {
+    private String line1;
+    private String line2;
+    private String city;
+    private String state;
+    private String zip;
+    private String country;
+
+    public Address(String line1, String line2, String city, String state, String zip, String country){
+        this.line1 = line1;
+        this.line2 = line2;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.country = country;
+    }
+
+    public String printAddress() {
+        return line1 + ", " + line2 + ", " + city + ", " + state + ", " + zip + ", " + country;
+    }
+
+    public void setAddress(String line1, String line2, String city, String state, String zip, String country) {
+        this.line1 = line1;
+        this.line2 = line2;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.country = country;
+    }
+
+    
+}
