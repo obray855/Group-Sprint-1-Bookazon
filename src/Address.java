00a547490909010f8398c02aaf127062ea4110f9
obray855 <81979857+obray855@users.noreply.github.com>
2024-09-30 14:59:55 -0400

    Initialized addresses
    Fixed null pointer problems in the bookazon class when setting addresses.
    
    Co-Authored-By: Yelaman Moldagali <115030149+yelagman@users.noreply.github.com>

--- src/Address.java
+++ src/Address.java
@@ -6,6 +6,8 @@ public class Address {
     private String zip;
     private String country;
 
+    public Address(){}
+
     public Address(String line1, String line2, String city, String state, String zip, String country){
         this.line1 = line1;
         this.line2 = line2;
